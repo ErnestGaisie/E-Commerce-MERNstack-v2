d4f9c929e05b74ab9edcc156569fde7cae60f3bb
Ernest Gaisie <ernestyawgaisie@gmail.com>
2021-01-04 19:06:53 +0000

    Cart functionality done

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,6 +1,10 @@
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
-export const cartReducer = (state = { cartItems: [] }, action) => {
+const initialState = {
+  cartItems: [],
+};
+
+export const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
@@ -20,9 +24,14 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
           cartItems: [...state.cartItems, item],
         };
       }
-    default:
-      break;
-  }
 
-  return { ...state };
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((x) => x.product !== action.payload),
+      };
+
+    default:
+      return state;
+  }
 };
