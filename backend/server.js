91fae9bc224f5295569a6a4b7a6cd2ee2c8c8aaa
Ernest Gaisie <ernestyawgaisie@gmail.com>
2021-01-05 16:10:07 +0000

    User authentication endpoint

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from "colors";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -11,6 +12,8 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.originalUrl);
@@ -22,6 +25,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
