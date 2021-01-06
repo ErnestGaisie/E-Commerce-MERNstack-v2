b1792172fea6e13808c521a0526baf7d6fdec2a3
Ernest Gaisie <ernestyawgaisie@gmail.com>
2021-01-06 15:20:07 +0000

    User Login Screen & Fuctionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/homescreen/HomeScreen";
 import ProductScreen from "./screens/productscreen/ProductScreen";
 import CartScreen from "./screens/cartscreen/CartScreen";
+import LoginScreen from "./screens/loginscreen/LoginScreen";
 
 const App = () => {
   return (
@@ -13,9 +14,10 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <Route path="/" component={HomeScreen} exact />
+          <Route path="/login" component={LoginScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
 } from "../constants/userConstants";
 
 export const login = (email, password) => async (dispatch) => {
