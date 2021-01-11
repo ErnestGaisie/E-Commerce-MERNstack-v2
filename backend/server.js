e3c15fccb9da0281f817bff0a3480f732353fffe
Ernest Gaisie <ernestyawgaisie@gmail.com>
2021-01-11 13:25:29 +0000

    Order Controller & Route

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import colors from "colors";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import orderRoutes from "./routeS/orderRoutes.JS";
 
 dotenv.config();
 
@@ -26,6 +27,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.use(notFound);
 
