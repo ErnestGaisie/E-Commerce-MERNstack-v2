6169be9151455967fd24d9e170f7de256b1ac5a6
Ernest Gaisie <ernestyawgaisie@gmail.com>
2021-01-14 12:50:04 +0000

    Get Order by ID Endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -36,4 +36,21 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems };
+//@desc GET order by id
+//@route GET /api/orders/:id
+//@access Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await (await Order.findById(re.params.id)).populated(
+    "user",
+    "name email"
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+export { addOrderItems, getOrderById };
