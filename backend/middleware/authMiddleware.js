ceeb3d702521917676ea58963082e233c60cb248
Ernest Gaisie <ernestyawgaisie@gmail.com>
2021-01-15 15:32:38 +0000

    Admin Middleware & Get Users Endpoint

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -33,4 +33,13 @@ const protect = expressAsyncHandler(async (req, res, next) => {
   }
 });
 
-export { protect };
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next;
+  } else {
+    res.status(401);
+    throw new Error("Not authorized as admin");
+  }
+};
+
+export { protect, admin };
